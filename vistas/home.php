c89d3d9ad0b41f072b34c8d62c43f48cfc0e81bb
Junior Arias <158210736+JuniorArias02@users.noreply.github.com>
2024-06-07 12:13:58 -0400

    modificaciones de  la pestaña home

--- vistas/home.php
+++ vistas/home.php
@@ -1,4 +1,6 @@
 <div class="container is-fluid">
-	<h1 class="title">Home</h1>
-	<h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['nombre']." ".$_SESSION['apellido']; ?>!</h2>
+	<div class="center_container-dash">
+		<h1 class="title">Home</h1>
+		<h2 class="subtitle">¡Bienvenido <?php echo $_SESSION['nombre'] . " " . $_SESSION['apellido']; ?>!</h2>
+	</div>
 </div>
